d3e6e75e4390989c441317b07167e45eaae349cc
Tsangington <harry.tsang@code.berlin>
2023-01-09 14:32:11 +0100

    Added Navbar functionality Added profile.html

--- ovation_app/__init__.py
+++ ovation_app/__init__.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/about')
+@app.route('/landing')
 @app.route('/')
 def landing():
     return render_template('landing.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 def create_app():
     return app
