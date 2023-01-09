8faf924ed4d205ad0fe7f62dc92fcbb2d5d81618
Tsangington <harry.tsang@code.berlin>
2023-01-09 12:51:32 +0100

    Added __init__.py file, Added route to landing.html

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from flask import Flask
+from ovation_app import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ ovation_app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+def create_app():
+    return app
